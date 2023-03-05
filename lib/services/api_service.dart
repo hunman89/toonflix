d6a909b674f1e5643272db6c45bf3d6bda39e7ac
hunman89 <hunman89@gmail.com>
2023-03-05 22:37:00 +0900

    finish app

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -18,6 +20,30 @@ class ApiService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final res = await http.get(url);
+    if (res.statusCode == 200) {
+      return WebtoonDetailModel.fronJson(jsonDecode(res.body));
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getToonEpisodeById(String id) async {
+    List<WebtoonEpisodeModel> webtoonEpisodes = [];
+
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final res = await http.get(url);
+    if (res.statusCode == 200) {
+      final episodes = jsonDecode(res.body);
+      for (var episode in episodes) {
+        webtoonEpisodes.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return webtoonEpisodes;
     }
     throw Error();
   }
--- lib/widget/datail_screen.dart
+++ lib/widget/datail_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+import 'episode_widget.dart';
+
+class DetailScreen extends StatefulWidget {
   final String thumb, title, id;
 
   const DetailScreen({
@@ -11,6 +17,53 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences pref;
+  bool isLiked = false;
+
+  Future initPrefs() async {
+    pref = await SharedPreferences.getInstance();
+    final likedToons = pref.getStringList('likedToons');
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      await pref.setStringList('likedToons', []);
+    }
+  }
+
+  void onLikeButtonTab() async {
+    final likedToons = pref.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await pref.setStringList('likedToons', likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getToonEpisodeById(widget.id);
+    initPrefs();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -18,48 +71,108 @@ class DetailScreen extends StatelessWidget {
         elevation: 1,
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
+        actions: [
+          IconButton(
+              onPressed: onLikeButtonTab,
+              icon: Icon(
+                isLiked ? Icons.favorite : Icons.favorite_border,
+              ))
+        ],
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w600,
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
             children: [
-              Hero(
-                tag: id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 10,
-                          offset: const Offset(10, 10),
-                          color: Colors.black.withOpacity(0.5),
-                        )
-                      ]),
-                  child: Image.network(
-                    thumb,
-                    headers: const {
-                      "User-Agent":
-                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                    },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 10,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.5),
+                          )
+                        ],
+                      ),
+                      child: Image.network(
+                        widget.thumb,
+                        headers: const {
+                          "User-Agent":
+                              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                        },
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              FutureBuilder(
+                builder: (context, snapShot) {
+                  if (snapShot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapShot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          '${snapShot.data!.genre} / ${snapShot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        )
+                      ],
+                    );
+                  } else {
+                    return const Text("...");
+                  }
+                },
+                future: webtoon,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapShot) {
+                  if (snapShot.hasData) {
+                    return Column(
+                      children: [
+                        for (var episode in snapShot.data!)
+                          Episode(episode: episode, webtoonId: widget.id),
+                      ],
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              )
             ],
           ),
-        ],
+        ),
       ),
     );
   }
